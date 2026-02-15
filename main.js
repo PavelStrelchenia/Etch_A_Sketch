7b43474b9a765b35682b7d9e2d294c1fb0203e90
Pavel Strelchenia <pivlik5608@gmail.com>
2026-02-15 10:23:43 +0300

    Add main function

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 "use strict";
+
+const container = document.querySelector(".container");
+const btn = document.querySelector(".btn");
+const fragment = document.createDocumentFragment();
+
+btn.addEventListener("click", size);
+
+let totalContainer = 16 * 16;
+
+function size(num) {
+    num = +prompt("Enter new size");
+    totalContainer = num * num;
+}
+
+for (let i = 0; i < totalContainer; i++) {
+    const div = document.createElement("div");
+
+    div.style.width = "50px";
+    div.style.height = "50px";
+
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "black";
+    });
+
+    fragment.appendChild(div);
+}
+
+container.appendChild(fragment);
+
+container.style.display = "flex";
+container.style.width = "800px";
+container.style.flexWrap = "wrap";
