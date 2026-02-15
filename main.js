397bee8eaadfe1f5a3a703142578121f705160c0
PavelStrelchenia <sprrps@icloud.com>
2026-02-15 11:03:58 +0300

    Fix function for add squares

--- main.js
+++ main.js
@@ -2,32 +2,63 @@
 
 const container = document.querySelector(".container");
 const btn = document.querySelector(".btn");
-const fragment = document.createDocumentFragment();
 
-btn.addEventListener("click", size);
+function createField(size) {
+  container.innerHTML = "";
+  const fragment = document.createDocumentFragment();
+  const totalSquare = size * size;
+  const squareSize = 800 / size;
 
-let totalContainer = 16 * 16;
-
-function size(num) {
-    num = +prompt("Enter new size");
-    totalContainer = num * num;
-}
-
-for (let i = 0; i < totalContainer; i++) {
+  for (let i = 0; i < totalSquare; i++) {
     const div = document.createElement("div");
 
-    div.style.width = "50px";
-    div.style.height = "50px";
+    div.style.width = `${squareSize}px`;
+    div.style.height = `${squareSize}px`;
+    div.style.boxSizing = "border-box";
+    div.style.border = "1px solid grey";
+
+    div.style.transition = "filter 0.1s";
 
     div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "black";
+      if (!div.style.backgroundColor) {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+
+        div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+
+        div.dataset.brightness = "100";
+      } else {
+        let currentBrightness = parseInt(div.dataset.brightness);
+
+        if (currentBrightness > 0) {
+          currentBrightness -= 10;
+          div.dataset.brightness = currentBrightness;
+
+          div.style.filter = `brightness(${currentBrightness}%)`;
+        }
+      }
     });
 
     fragment.appendChild(div);
+  }
+  container.appendChild(fragment);
 }
 
-container.appendChild(fragment);
+btn.addEventListener("click", () => {
+  const userSizeField = +prompt("Enter new size: ");
+
+  if (userSizeField > 0 && userSizeField <= 100) {
+    createField(userSizeField);
+  } else {
+    alert("Please enter a number between 1 and 100");
+  }
+});
 
 container.style.display = "flex";
 container.style.width = "800px";
+container.style.height = "800px";
 container.style.flexWrap = "wrap";
+container.style.border = "2px solid gray";
+
+createField(16);
